00630b3c60d810f503c5c852ea556a2c2bbc60d7
Nitin Gumber <aroranitin486@gmail.com>
2023-10-28 14:59:56 +0530

    fix-menubar-mobile dynamically-data-js

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+function menuBar () {
+    document.getElementById("navl").style.right = "0px";
+}
+function hideMenu () {
+    document.getElementById("navl").style.left = "100%";
+}
+
+
+
+
+// Counter:- 
+const counters = document.querySelectorAll('.counter')
+counters.forEach((counter) => {
+        counter.innerHTML = 0
+
+        const updateCounter = () => {
+            const targetCount = +counter.getAttribute('data-target')
+
+            const startingCount = Number(counter.innerHTML)
+
+            const incr = targetCount / 100
+
+            if (startingCount < targetCount) {
+                counter.innerHTML = `${Math.round(startingCount + incr)}`
+                setTimeout(updateCounter, 100)
+            } else {
+                counter.innerHTML = targetCount
+            }
+        }
+        updateCounter()
+})
